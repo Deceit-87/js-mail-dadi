e3afc11c50a5c64a5fce870dc4a3ab9719799c9a
Deceit-87 <manuelfreund318@gmail.com>
2022-02-09 15:41:10 +0100

    prompt + array mail

---
+++ js/app.js
@@ -0,0 +1,38 @@
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+
+
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi ha il numero più alto.  (e se pari?)
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Che ci sia un array da qualche parte?
+// Se dobbiamo confrontare qualcosa che “cosa” ci serve?
+// Consigli del giorno:
+// 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+// 2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+// 3. si ma noi cosa vogliamo fare?
+// 4. torniamo a scrivere in italiano
+// 5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo “a mano”
+
+
+
+                //  ESERCIZIO 1
+
+
+           let userMail = prompt('inserisci qui la tua mail')
+
+            console.log(userMail)
+
+            let registerUsers= ['gianni.pinotto@gmail.com','evaquo.felice@libero.com','mario.rossi@alice.it','ciccio.panza@yahoo.it']
+            
+            console.log(registerUsers.length)
+
+            for (let i = 0; i < registerUsers.length; i++) {
+                const element = [i];
+                
+            }
+
+
